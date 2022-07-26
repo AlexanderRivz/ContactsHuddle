4863c04b83e07748f5842e0a4d1c76bd2c66fa4f
Christian Rivera <carivas@applaudostudios.com>
2022-07-25 18:08:40 -0600

    :tada: ContactsHuddle - Adds base project

---
+++ ContactsHuddle/ContactsHuddle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsHuddle
+//
+//  Created by Alexander Rivera on 25/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
